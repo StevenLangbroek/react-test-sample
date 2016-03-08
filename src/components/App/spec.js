c63e23f87a7deccb7ad87471312b18c06172483d
Steven Langbroek <steven@digitaaldier.nl>
2016-03-08 17:53:10 +0100

    Add static props and babel transform for them

--- src/components/App/spec.js
+++ src/components/App/spec.js
@@ -5,12 +5,12 @@ const render = createRenderer(App);
 
 describe('<App />', () => {
   it('should have a correct title', () => {
-    const { output } = render({
+    const { output, props } = render({
       title: 'Hello world!'
     });
 
     const subject = output.querySelector('h1').textContent;
-    const expectation = 'Hello world!';
+    const expectation = props.title;
 
     expect(subject, 'has title').to.equal(expectation);
   });
